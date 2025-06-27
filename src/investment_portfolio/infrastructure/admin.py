8c0af93495c27aa180e9f76c7e193b4fe84415bb
Алексей Лесков <alexleskov2000@gmail.com>
2025-06-27 18:50:33 +0800

    Create user investment portfolio use case

--- src/investment_portfolio/infrastructure/admin.py
+++ src/investment_portfolio/infrastructure/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import InvestmentPortfolioModel
+
+
+class InvestmentPortfolioModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at", "updated_at")
+
+
+admin.site.register(InvestmentPortfolioModel, InvestmentPortfolioModelAdmin)
