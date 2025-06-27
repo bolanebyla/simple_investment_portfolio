aed29308c7da384d8aed48827292452e5b82a408
Алексей Лесков <alexleskov2000@gmail.com>
2025-06-27 18:41:12 +0800

    Create user investment portfolio use case

--- src/investment_portfolio/infrastructure/repositories/investment_portfolio_repo.py
+++ src/investment_portfolio/infrastructure/repositories/investment_portfolio_repo.py
@@ -6,5 +6,6 @@ from investment_portfolio.infrastructure.models import InvestmentPortfolioModel
 class InvestmentPortfolioRepoImpl(InvestmentPortfolioRepo):
     def add(self, investment_portfolio: InvestmentPortfolio) -> None:
         InvestmentPortfolioModel.objects.create(
-            id=investment_portfolio.id, title=investment_portfolio.title
+            id=investment_portfolio.id,
+            title=investment_portfolio.title,
         )
